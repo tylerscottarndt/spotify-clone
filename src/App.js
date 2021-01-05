44adc100c166c99639e0522acb460aa0445aabef
Tyler Arndt <Tyler.Arndt@motorolasolutions.com>
2021-01-04 21:13:25 -0600

    successfully set token for spotify

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
     const [token, setToken] = useState(null);
@@ -14,6 +17,10 @@ function App() {
 
         if (_token) {
             setToken(_token);
+            spotify.setAccessToken(_token);
+            spotify.getMe().then((user) => {
+                console.log('USER>>>', user);
+            });
         }
 
         console.log('I HAVE A TOKEN>>> ', token);
