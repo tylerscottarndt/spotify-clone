bc69fc6b11253023b03a7376e529de25fe5bc6d5
Tyler Arndt <Tyler.Arndt@motorolasolutions.com>
2021-01-04 20:49:41 -0600

    login functionality and token retrieval

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Login from './Login';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+    const [token, setToken] = useState(null);
+
+    // run code based on a given condition
+    useEffect(() => {
+        const hash = getTokenFromUrl();
+        window.location.hash = '';
+        const _token = hash.access_token;
+
+        if (_token) {
+            setToken(_token);
+        }
+
+        console.log('I HAVE A TOKEN>>> ', token);
+    }, []);
+
     return (
-        <div className="app">
-            <h1>Hello World!</h1>
-        </div>
+        <div className="app">{token ? <h1>I am logged in</h1> : <Login />}</div>
     );
 }
 
