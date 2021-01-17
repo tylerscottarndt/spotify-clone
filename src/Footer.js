f4c4c9c14f36b81a4cbc4c42bc9df572ea03e8b9
Tyler Arndt <Tyler.Arndt@motorolasolutions.com>
2021-01-17 17:42:14 -0600

    add footer-left ui

--- src/Footer.js
+++ src/Footer.js
@@ -13,7 +13,15 @@ function Footer() {
     return (
         <div className="footer">
             <div className="footer__left">
-                <p>Album and song details</p>
+                <img
+                    className="footer__albumLogo"
+                    src="https://upload.wikimedia.org/wikipedia/en/d/da/Third_eye_blind_self_titled.jpg"
+                    alt=""
+                />
+                <div className="footer__songInfo">
+                    <h4>Motorcycle Driveby</h4>
+                    <p>Third Eye Blind</p>
+                </div>
             </div>
             <div className="footer__center">
                 <ShuffleIcon className="footer__green" />
