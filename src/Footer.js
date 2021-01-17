109aa1a01830d06034b1d942a63ab23732c7eb9d
Tyler Arndt <Tyler.Arndt@motorolasolutions.com>
2021-01-17 17:05:11 -0600

    create footer middle

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import './Footer.css';
-
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import ShuffleIcon from '@material-ui/icons/Shuffle';
+import RepeatIcon from '@material-ui/icons/Repeat';
 function Footer() {
     return (
         <div className="footer">
-            <h1>I'm a footer</h1>
+            <div className="footer__left">
+                <p>Album and song details</p>
+            </div>
+            <div className="footer__center">
+                <ShuffleIcon className="footer__green" />
+                <SkipPreviousIcon className="footer__icon" />
+                <PlayCircleOutlineIcon
+                    className="footer__icon"
+                    style={{ fontSize: 50 }}
+                />
+                <SkipNextIcon className="footer_icon" />
+                <RepeatIcon className="footer__green" />
+            </div>
+            <div className="footer__right">
+                <p>Volume controls</p>
+            </div>
         </div>
     );
 }
