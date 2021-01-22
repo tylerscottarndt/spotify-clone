bed6cc9f10c270efe77ad94df1408fc845e0b475
Tyler Arndt <Tyler.Arndt@motorolasolutions.com>
2021-01-21 21:31:27 -0600

    add body icons

--- src/Body.js
+++ src/Body.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './Body.css';
 import Header from './Header.js';
 import { useDataLayerValue } from './DataLayer';
+import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
 // a little prop drilling for 'spotify'
 function Body({ spotify }) {
@@ -18,6 +21,17 @@ function Body({ spotify }) {
                     <strong> PLAYLIST</strong>
                     <h2>Discover Weekly</h2>
                     <p>{discover_weekly?.description}</p>
+                </div>
+            </div>
+
+            <div className="body__songs">
+                <div className="body__icons">
+                    <PlayCircleFilledIcon className="body__shuffle body__greenIcon" />
+                    <FavoriteIcon
+                        className="body__greenIcon"
+                        fontSize="large"
+                    />
+                    <MoreHorizIcon />
                 </div>
             </div>
         </div>
